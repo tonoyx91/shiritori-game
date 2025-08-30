139048eb151932ba739adccbae760b0f5c0c21bf
tonoyx91 <tonoygscian@gmail.com>
2025-08-30 17:22:52 +0600

    Done 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Shiritori Game | Abrar",
+  title: "Shiritori Game by tonoyx91",
   description: "A Japanese Word Game",
 };
 
